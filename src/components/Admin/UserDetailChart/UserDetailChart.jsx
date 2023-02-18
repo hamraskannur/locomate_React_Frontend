639ae3e68ca515d42fabec5ada2ffef330b75d26
hamraskannur <kkhamras741@gmail.com>
2023-02-18 09:15:36 +0530

    changed chart type

--- src/components/Admin/UserDetailChart/UserDetailChart.jsx
+++ src/components/Admin/UserDetailChart/UserDetailChart.jsx
@@ -27,7 +27,7 @@ const UserDetailChart = ({userGraphCategories,userGraphData}) => {
         <Chart
           options={state.options}
           series={state.series}
-          type="bar"
+          type="line"
           width="600"
         />
       </div>
