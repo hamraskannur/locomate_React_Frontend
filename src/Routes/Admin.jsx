fb4211322eeaa4702791405b873314b3cc6c86f3
hamraskannur <kkhamras741@gmail.com>
2023-09-20 20:32:22 +0530

    updated

--- src/Routes/Admin.jsx
+++ src/Routes/Admin.jsx
@@ -2,9 +2,7 @@ import React, { lazy, Suspense } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import AdminProtectRouter from '../components/Admin/Routes/AdminProtectRouter';
 import AdminPublicRoute from '../components/Admin/Routes/AdminPublicRoute';
-import ErrorPage from "../pages/admin/404page";
 
-// Lazy-loaded components
 const Dashboard = lazy(() => import('../pages/admin/DashboardPage'));
 const ShowUsers = lazy(() => import('../pages/admin/ShowUsers'));
 const Login = lazy(() => import('../pages/admin/Login'));
@@ -13,23 +11,24 @@ const OneUser = lazy(() => import('../pages/admin/OneUser'));
 const OnePostReport = lazy(() => import('../pages/admin/OnePostReport'));
 const Notifications = lazy(() => import('../pages/admin/Notifications'));
 const AllPost = lazy(() => import('../pages/admin/AllPost'));
+const ErrorPage = lazy(() => import('../pages/admin/404page'));
 
 function Admin() {
   return (
-    <>
+    <Suspense fallback={<div>Loading...</div>}>
       <Routes>
-        <Route path='/' element={<AdminProtectRouter><Suspense fallback={<div>Loading...</div>}><Dashboard /></Suspense></AdminProtectRouter>} />
-        <Route path='/allUsers' element={<AdminProtectRouter><Suspense fallback={<div>Loading...</div>}><ShowUsers /></Suspense></AdminProtectRouter>} />
-        <Route path='/login' element={<AdminPublicRoute><Suspense fallback={<div>Loading...</div>}><Login /></Suspense></AdminPublicRoute>} />
-        <Route path='/reportedPost' element={<AdminProtectRouter><Suspense fallback={<div>Loading...</div>}><ReportedPost /></Suspense></AdminProtectRouter>} />
-        <Route path='/oneUser' element={<AdminProtectRouter><Suspense fallback={<div>Loading...</div>}><OneUser /></Suspense></AdminProtectRouter>} />
-        <Route path='/OnePostReport' element={<AdminProtectRouter><Suspense fallback={<div>Loading...</div>}><OnePostReport /></Suspense></AdminProtectRouter>} />
-        <Route path='/userProfile' element={<AdminProtectRouter><Suspense fallback={<div>Loading...</div>}><OneUser /></Suspense></AdminProtectRouter>} />
-        <Route path='/notifications' element={<AdminProtectRouter><Suspense fallback={<div>Loading...</div>}><Notifications /></Suspense></AdminProtectRouter>} />
-        <Route path='/allPost' element={<AdminProtectRouter><Suspense fallback={<div>Loading...</div>}><AllPost /></Suspense></AdminProtectRouter>} />
+        <Route path="/" element={<AdminProtectRouter><Dashboard /></AdminProtectRouter>} />
+        <Route path="/allUsers" element={<AdminProtectRouter><ShowUsers /></AdminProtectRouter>} />
+        <Route path="/login" element={<AdminPublicRoute><Login /></AdminPublicRoute>} />
+        <Route path="/reportedPost" element={<AdminProtectRouter><ReportedPost /></AdminProtectRouter>} />
+        <Route path="/oneUser" element={<AdminProtectRouter><OneUser /></AdminProtectRouter>} />
+        <Route path="/OnePostReport" element={<AdminProtectRouter><OnePostReport /></AdminProtectRouter>} />
+        <Route path="/userProfile" element={<AdminProtectRouter><OneUser /></AdminProtectRouter>} />
+        <Route path="/notifications" element={<AdminProtectRouter><Notifications /></AdminProtectRouter>} />
+        <Route path="/allPost" element={<AdminProtectRouter><AllPost /></AdminProtectRouter>} />
         <Route path="/*" element={<ErrorPage />} />
       </Routes>
-    </>
+    </Suspense>
   );
 }
 
--- src/components/User/Suggestion/Suggestion.jsx
+++ src/components/User/Suggestion/Suggestion.jsx
@@ -7,46 +7,52 @@ import Avatars from "../avatar/Avatar";
 const Suggestion = ({ user }) => {
   const sideBar = useSelector((state) => state?.sideBar?.sideBar);
   const userId = useSelector((state) => state?.user?.user._id);
-  const navigate = useNavigate()  
+  const navigate = useNavigate();
 
-  const getAccountPage = async (user) => {
- 
+  const getAccountPage = async (e,user) => {
+    e.preventDefault();
     if (userId === user) {
-
       navigate("/myAccount");
     } else {
-      
-      navigate('/friendsAccount',{ state: { userId: user,  }} );
+      navigate("/friendsAccount", { state: { userId: user } });
     }
-  
-};
+  };
   return (
     <>
-      <div onClick={()=>getAccountPage( user._id)} className="flex items-center gap-2 py-2 m-4 px-2 hover:bg-[#bbc0c7] hover:scale-105">
-        <div className='cursor-pointer'>
-          <Avatars
-            img={user.ProfileImg}
-          />
+      <div
+        onClick={(e) => getAccountPage(e,user._id)}
+        className="flex items-center gap-2 py-2 m-4 px-2 hover:bg-[#bbc0c7] hover:scale-105"
+      >
+        <div className="cursor-pointer">
+          <Avatars img={user.ProfileImg} />
         </div>
-        <div  className="px-2 font-semibold cursor-pointer">
+        <div className="px-2 font-semibold cursor-pointer">
           <h2> {user.username}</h2>
           <p className="text-gray-400 text-sm p-0">{user.name}</p>
         </div>
         {!sideBar && (
           <div className="ml-auto flex duration-900 ">
             <div>
-              <h1 className="align-middle ml-8 font-semibold">{user?.Followers?.length?user?.Followers?.length:0}</h1>
+              <h1 className="align-middle ml-8 font-semibold">
+                {user?.Followers?.length ? user?.Followers?.length : 0}
+              </h1>
               <div className="text-slate-700 p-1 rounded-md px-2 flex">
-                <div>              {React.createElement(BsFillPeopleFill, { size: "20" })}
-</div>
+                <div>
+                  {" "}
+                  {React.createElement(BsFillPeopleFill, { size: "20" })}
+                </div>
                 followers
               </div>
             </div>
             <div>
-              <h1 className="align-middle ml-8 font-semibold">{user?.Following?.length ?user?.Following?.length:0}</h1>
+              <h1 className="align-middle ml-8 font-semibold">
+                {user?.Following?.length ? user?.Following?.length : 0}
+              </h1>
               <div className="text-slate-700 p-1 rounded-md px-2 flex">
-                <div>              {React.createElement(BsFillPeopleFill, { size: "20" })}
-</div>
+                <div>
+                  {" "}
+                  {React.createElement(BsFillPeopleFill, { size: "20" })}
+                </div>
                 following
               </div>
             </div>
